af8854c9dfe5d2e44a90346f2416de9fc9c9df8a
Rutwik187 <rutwikshinde34@gmail.com>
2024-01-18 11:12:14 +0530

    fixing deploymet issues: aboutus.js

--- app/(website)/about/about.js
+++ app/(website)/about/about.js
@@ -3,7 +3,7 @@ import { urlForImage } from "@/lib/sanity/image";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function About({ authors, settings }) {
+export default function About({ authors }) {
   return (
     <Container>
       <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
